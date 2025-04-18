1e5b622619fda554baecc95db8bc505dd73f8a4c
Violeta Rodriguez <vio.ayelen@gmail.com>
2025-04-18 19:49:42 -0300

    delete images

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,19 +1,8 @@
 "use client";
 
-import { Button } from "primereact/button";
-import { Card } from "primereact/card";
-
 export default function Home() {
   return (
-    <main className="p-6">
-      <Card title="¡Hola PrimeReact!">
-        <p className="m-0">
-          Esta tarjeta usa <strong>PrimeReact</strong> y <strong>PrimeFlex</strong>.
-        </p>
-        <div className="mt-4 flex justify-content-center">
-          <Button label="Probando botón" icon="pi pi-check" />
-        </div>
-      </Card>
-    </main>
+    <>
+    </>
   );
 }
